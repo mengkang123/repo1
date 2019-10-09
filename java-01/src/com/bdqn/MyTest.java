974f4e2ca5e4c47630d6d319f85639fe74a34c0f
mengkang <mengkang@git.com>
2019-10-09 20:54:26 +0800

    uuuuuuuuuuuuuuu

---
+++ java-01/src/com/bdqn/MyTest.java
@@ -0,0 +1,12 @@
+package com.bdqn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyTest {
+    public static void main(String[] args) {
+        List<String>  lists=new ArrayList<>();
+        lists.add("1");
+        lists.add("2");
+}
+}
