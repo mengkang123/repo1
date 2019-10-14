b4dc39f9d5db47bbfedf8461308afcae20d5066e
mengkang <mengkang@git.com>
2019-10-14 18:21:59 +0800

    把本地工程添加到git

---
+++ git-project/src/cn/bdqn/Text.java
@@ -0,0 +1,17 @@
+package cn.bdqn;/**
+ * className:Text
+ * package:cn.bdqn
+ * description:
+ *
+ * @Data:2019/10/14 18:12
+ * @Author:mengkang
+ */
+
+/**
+ * author:user
+ */
+public class Text {
+    public static void main(String[] args) {
+        int q=1;
+    }
+}
